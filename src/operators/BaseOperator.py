bdfa1c0129cd6dec336430f8e193a0568469c740
Gustavo Maia <vm.gustavo31@gmail.com>
2022-06-19 18:09:32 -0300

    start add operators

---
+++ src/operators/BaseOperator.py
@@ -0,0 +1,24 @@
+from typing import Tuple
+from abc import ABC, abstractmethod
+
+
+class BaseOperator(ABC):
+    def __init__(self, paths: Tuple[str, str]):
+        self.paths = paths
+
+    @staticmethod
+    @abstractmethod
+    def _loader(path: str):
+        raise NotImplementedError
+
+    @abstractmethod
+    def groupby(self, dtype: str):
+        raise NotImplementedError
+
+    @abstractmethod
+    def join(self, dtype: str):
+        raise NotImplementedError
+
+    @abstractmethod
+    def aggregate(self, dtype: str):
+        raise NotImplementedError
