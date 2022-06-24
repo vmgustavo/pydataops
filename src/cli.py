ac044e557685a51901a8985b743a140b171d4b91
Gustavo Maia <vm.gustavo31@gmail.com>
2022-06-23 22:35:17 -0300

    Merge pull request #2
    * add groups argument to create data
    
    * add todo
    
    * use new definition of groups from create data
    
    * add group argument to path
    
    * add group argument to path to data evaluation
    
    * add pandas groupby operation

--- src/cli.py
+++ src/cli.py
@@ -28,23 +28,57 @@ def cli(ctx, directory):
 
 
 @cli.command()
-@click.option("-r", "--rows", type=int, required=True, multiple=True, help="Number of rows")
-@click.option("--overwrite", is_flag=True, help="Overwrite existing data files")
+@click.option(
+    "--rows",
+    type=int,
+    required=True,
+    multiple=True,
+    help="Number of rows",
+)
+@click.option(
+    "--groups",
+    required=True,
+    multiple=True,
+    help="Percentage of the number of rows to use as number of groups or absolute number of groups",
+)
+@click.option(
+    "--overwrite",
+    is_flag=True,
+    help="Overwrite existing data files",
+)
 @click.pass_context
-def create_data(ctx, rows, overwrite):
-    from src import CreateData
+def create_data(ctx, rows, groups, overwrite):
+    from src import DataPath, CreateData
 
     logger = logging.getLogger("create-data")
-    for nrows in rows:
-        if (
-            overwrite
-            or (not os.path.exists(ctx.obj["filepaths"][0]))
-            or (not os.path.exists(ctx.obj["filepaths"][0]))
-        ):
-            logger.info(f"Generating new random data with {nrows} rows")
-            CreateData(rows=nrows, datadir=ctx.obj["datadir"]).gen()
+
+    for groups_arg in map(float, groups):
+        if groups_arg.is_integer():
+            groups_num = [int(groups_arg)] * len(rows)
         else:
-            logger.info("Random data already exists. Skip create-data step")
+            groups_num = [int(groups_arg * row) for row in rows]
+
+        for nrows, ngroups in zip(rows, groups_num):
+            filepaths = (
+                DataPath(ctx.obj["directory"], nrows, ngroups, groups_arg).primary(),
+                DataPath(ctx.obj["directory"], nrows, ngroups, groups_arg).secondary(),
+            )
+
+            if (
+                overwrite
+                or (not os.path.exists(filepaths[0]))
+                or (not os.path.exists(filepaths[1]))
+            ):
+                logger.info(f"Generating new random data with {nrows} rows and {ngroups} groups.")
+                CreateData(rows=nrows, groups=ngroups, datadir=ctx.obj["directory"]).gen(
+                    primary=filepaths[0], secondary=filepaths[1]
+                )
+            else:
+                logger.info(
+                    "Skip data creation."
+                    + f" Data with {nrows} rows and {ngroups} groups already exists."
+                    + f" Paths: {list(map(str, filepaths))}"
+                )
 
 
 @cli.command()
@@ -90,8 +124,7 @@ def create_data(ctx, rows, overwrite):
     "--groups",
     required=True,
     multiple=True,
-    type=int,
-    help="Number of groups of the datasets",
+    help="Percentage of the number of rows to use as number of groups or absolute number of groups",
 )
 @click.option(
     "--samples",
@@ -113,65 +146,71 @@ def eval_library(ctx, library, groupby, join, aggregate, rows, groups, samples):
     collector = Collector()
 
     mapper = {elem.__name__.lower(): elem for elem in BaseOperator.__subclasses__()}
-    for curr_lib, curr_rows, curr_groups in product(library, rows, groups):
-        datapath = DataPath(ctx.obj["directory"], curr_rows, curr_groups)
+    for groups_arg in map(float, groups):
+        if groups_arg.is_integer():
+            groups_num = [int(groups_arg)] * len(rows)
+        else:
+            groups_num = [int(groups_arg * row) for row in rows]
 
-        if (not datapath.primary().exists()) or (not datapath.secondary().exists()):
-            logger.warning(
-                f"The combination of library '{curr_lib}', rows '{curr_rows}' and groups '{curr_groups}'"
-                + " has no available dataset. The current processing step will be skipped."
+        for curr_lib, (curr_rows, curr_groups) in product(library, zip(rows, groups_num)):
+            datapath = DataPath(ctx.obj["directory"], curr_rows, curr_groups, groups_arg)
+
+            if (not datapath.primary().exists()) or (not datapath.secondary().exists()):
+                logger.warning(
+                    f"The combination of library '{curr_lib}', rows '{curr_rows}' and groups '{curr_groups}'"
+                    + " has no available dataset. The current processing step will be skipped."
+                )
+                continue
+
+            dataset_p = str(datapath.primary())
+            dataset_s = str(datapath.secondary())
+            assert datapath.primary().exists()
+
+            curr_instance = mapper[f"{curr_lib}operator"](
+                paths=(datapath.primary(), datapath.secondary())
             )
-            continue
 
-        dataset_p = str(datapath.primary())
-        dataset_s = str(datapath.secondary())
-        assert datapath.primary().exists()
-
-        curr_instance = mapper[f"{curr_lib}operator"](
-            paths=(datapath.primary(), datapath.secondary())
-        )
-
-        for curr_dtype in tqdm(groupby, ncols=80, desc="GroupBy"):
-            for _ in tqdm(range(samples), desc=f"{curr_dtype}"):
-                exec_time = curr_instance.groupby(curr_dtype)
-                collector.save(
-                    EvalData(
-                        library=curr_lib,
-                        operation="groupby",
-                        col_dtype=curr_dtype,
-                        time=exec_time,
-                        dataset_p=dataset_p,
-                        dataset_s=None,
+            for curr_dtype in tqdm(groupby, ncols=80, desc="GroupBy"):
+                for _ in tqdm(range(samples), desc=f"{curr_dtype}"):
+                    exec_time = curr_instance.groupby(curr_dtype)
+                    collector.save(
+                        EvalData(
+                            library=curr_lib,
+                            operation="groupby",
+                            col_dtype=curr_dtype,
+                            time=exec_time,
+                            dataset_p=dataset_p,
+                            dataset_s=None,
+                        )
                     )
-                )
 
-        for curr_dtype in tqdm(join, ncols=80, desc="Join"):
-            for _ in tqdm(range(samples), ncols=80, desc=f"{curr_dtype}"):
-                exec_time = curr_instance.join(curr_dtype)
-                collector.save(
-                    EvalData(
-                        library=curr_lib,
-                        operation="join",
-                        col_dtype=curr_dtype,
-                        time=exec_time,
-                        dataset_p=dataset_p,
-                        dataset_s=dataset_s,
+            for curr_dtype in tqdm(join, ncols=80, desc="Join"):
+                for _ in tqdm(range(samples), ncols=80, desc=f"{curr_dtype}"):
+                    exec_time = curr_instance.join(curr_dtype)
+                    collector.save(
+                        EvalData(
+                            library=curr_lib,
+                            operation="join",
+                            col_dtype=curr_dtype,
+                            time=exec_time,
+                            dataset_p=dataset_p,
+                            dataset_s=dataset_s,
+                        )
                     )
-                )
 
-        for curr_dtype in tqdm(aggregate, ncols=80, desc="Aggregate"):
-            for _ in tqdm(range(samples), ncols=80, desc=f"{curr_dtype}", leave=False):
-                exec_time = curr_instance.aggregate(curr_dtype)
-                collector.save(
-                    EvalData(
-                        library=curr_lib,
-                        operation="aggregate",
-                        col_dtype=curr_dtype,
-                        time=exec_time,
-                        dataset_p=dataset_p,
-                        dataset_s=None,
+            for curr_dtype in tqdm(aggregate, ncols=80, desc="Aggregate"):
+                for _ in tqdm(range(samples), ncols=80, desc=f"{curr_dtype}", leave=False):
+                    exec_time = curr_instance.aggregate(curr_dtype)
+                    collector.save(
+                        EvalData(
+                            library=curr_lib,
+                            operation="aggregate",
+                            col_dtype=curr_dtype,
+                            time=exec_time,
+                            dataset_p=dataset_p,
+                            dataset_s=None,
+                        )
                     )
-                )
 
 
 def main():
