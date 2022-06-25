b2d6623c2e20ff094d9ee71a27f2e981bae8a0e4
Gustavo Maia <vm.gustavo31@gmail.com>
2022-06-25 17:34:02 -0300

    Merge pull request #7
    * add vaex

--- src/operators/__init__.py
+++ src/operators/__init__.py
@@ -1,4 +1,5 @@
 from .BaseOperator import BaseOperator
+from .VaexOperator import VaexOperator
 from .PandasOperator import PandasOperator
 from .PySparkOperator import PySparkOperator
 from .ModinRayOperator import ModinRayOperator
