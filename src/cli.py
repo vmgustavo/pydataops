44834150b2d5bc56fec3449d67a63ef743abc875
Gustavo Maia <vm.gustavo31@gmail.com>
2022-06-29 23:13:37 -0300

    set logging level to debug

--- src/cli.py
+++ src/cli.py
@@ -10,7 +10,7 @@ logging.basicConfig(
     filemode="a",
     format="%(asctime)s | %(levelname)-8s | %(name)s : %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
-    level=logging.INFO,
+    level=logging.DEBUG,
 )
 
 
